ea6150f9fd8f690dd77e2da9990f16e60b16b749
Fabrice Bellamy <12b@distrilab.fr>
2025-03-27 20:05:28 +0100

    NixOS support - 5th try

--- default.nix
+++ default.nix
@@ -1,23 +1,9 @@
 { pkgs ? import <nixpkgs> { }, src ? ./. }:
 
-let 
-  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-  theSource = src;
-in
+let theSource = src; in
 pkgs.rustPlatform.buildRustPackage rec {
-  pname = manifest.name;
-  version = manifest.version;
-  src = pkgs.lib.cleanSource ./.;
-  cargoLock.lockFile = ./Cargo.lock;
-  #src = pkgs.lib.cleanSource "${theSource}";
-  #cargoLock.lockFile = "${theSource}/Cargo.lock";
+  pname = "hosting-farm";
+  version = "0.1.1";
+  src = pkgs.lib.cleanSource "${theSource}";
+  cargoLock.lockFile = "${theSource}/Cargo.lock";
 }
-
-
-# version with hard-coded package name and version
-# pkgs.rustPlatform.buildRustPackage rec {
-#   pname = "hosting-farm";
-#   version = "0.1.0";
-#   cargoLock.lockFile = ./Cargo.lock;
-#   src = pkgs.lib.cleanSource ./.;
-# }
